97f98081c56f058f3e2229af1c4ea57779a44721
sadanandrudraiah <sadanand.rud@gmail.com>
2018-08-07 12:28:08 +0530

    initial

---
+++ src/main/java/com/accenture/demo/demo/App.java
@@ -0,0 +1,13 @@
+package com.accenture.demo.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
